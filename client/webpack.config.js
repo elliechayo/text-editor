ce52e9014179b638b75163ea0669b046b2a5144a
Elinor Chayo <elliechayo@gmail.com>
2023-06-19 14:21:55 -0400

    Added and configure workbox plugins for a service worker and manifest file, the CSS loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,44 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new WebpackPwaManifest({
+        filename: "manifest.json",
+        name: "JATE",
+        fingerprints: false,
+        short_name: "JATE",
+        description: "Just Another Text Editor",
+        background_color: "#000000",
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          exclude: /node_modules/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.js$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
       ],
     },
   };
